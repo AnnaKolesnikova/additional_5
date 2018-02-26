8e933a0133d9afa3706a2d50cfc4b3f65d444ceb
Anna <ani.kolesnikova@gmail.com>
2018-02-26 22:15:08 +0300

    solution additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) {
+    return false;
+  }
+
+  var stringForLoop = '',
+    savedString = '';
+
+  while (true) {
+
+    stringForLoop = '';
+    for (var i = 0; i < str.length; i++) {
+
+
+      for (var index = 0; index < bracketsConfig.length; index++) {
+        var bracketOp = bracketsConfig[index][0];
+        var bracketCl = bracketsConfig[index][1];
+
+        needContinue = false;
+
+        if (str[i] == bracketOp && str[i + 1] == bracketCl) {
+          needContinue = true;
+          break;
+        }
+      }
+      if (needContinue) {
+        i++
+        continue;
+      }
+      stringForLoop += str[i];
+    }
+
+    if (savedString == stringForLoop) {
+      break;
+    }
+    savedString = stringForLoop;
+    str = stringForLoop;
+
+  }
+  return (stringForLoop.length == 0) ? true : false;
+
 }
